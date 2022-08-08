050dae64a672d36b9d0438971f30cd8ab26ab5c6
Pete <mooshmore@gmail.com>
2022-08-08 12:53:25 +0200

    Add project files.

---
+++ Projects/ContextMenuTest/ContextMenuTest/MainWindowViewModel.cs
@@ -0,0 +1,64 @@
+﻿using PlaylistSaver.ProgramData.Bases;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ContextMenuTest
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            DisplayNameCommand = new RelayCommand(DisplayName);
+            DisplaySurnameCommand = new RelayCommand(DisplaySurname);
+
+            PeopleList = new List<Person>();
+
+            PeopleList.Add(new Person("Julie", "Azures"));
+            PeopleList.Add(new Person("Mack", "McMack"));
+            PeopleList.Add(new Person("Josh", "Broccoli"));
+
+            UpdateCurrentObjectCommand = new RelayCommand(UpdateCurrentObject);
+        }
+
+        private void UpdateCurrentObject(object person)
+        {
+            CurrentPerson = (Person)person;
+        }
+
+        private Person CurrentPerson { get; set; }
+
+        public List<Person> PeopleList { get; set; }
+
+        public void DisplayName()
+        {
+            MessageBox.Show("Name: " + CurrentPerson.Name);
+        }
+
+        public void DisplaySurname()
+        {
+            MessageBox.Show("Name: " + CurrentPerson.Surname);
+        }
+
+        public RelayCommand DisplayNameCommand { get; }
+        public RelayCommand DisplaySurnameCommand { get; }
+        public RelayCommand UpdateCurrentObjectCommand { get; }
+    }
+
+    public class Person
+    {
+        public Person(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
