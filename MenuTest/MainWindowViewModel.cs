1b86414b01d495dad79095b9d4341d3d7b6bf23d
Pete <mooshmore@gmail.com>
2022-09-16 18:18:33 +0200

    Writeable bitmap

---
+++ MenuTest/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using PlaylistSaver.ProgramData.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuTest
+{
+    public class MainWindowViewModel
+    {
+        public RelayCommand AfsluitenCommand { get; }
+        public RelayCommand BoekBeheerCommand { get; }
+        public RelayCommand AuteurBeheerCommand { get; }
+        public RelayCommand UitgeverBeheerCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            AfsluitenCommand = new RelayCommand(Afsluiten);
+            AfsluitenCommand = new RelayCommand(BoekBeheer);
+            AfsluitenCommand = new RelayCommand(AuteurBeheer);
+            AfsluitenCommand = new RelayCommand(UitgeverBeheer);
+        }
+
+        private void UitgeverBeheer()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AuteurBeheer()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BoekBeheer()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Afsluiten()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WriteableBitmapTest/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using ViewModelBaseNamespace;
+
+namespace WriteableBitmapTest
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+
+
+            // Using the relative path with a BitmapImage works, so the relative path is correct
+            //Uri relativePath = new Uri(@"Images\missingProfile.jpg", UriKind.Relative);
+            //Image = new BitmapImage(relativePath);
+
+            // Using a absolute path with a WriteableBitmap also works
+            //Uri absolutePath = new Uri(@"C:\Users\mooshmore\Downloads\missingProfile.jpg", UriKind.Absolute);
+            //BitmapImage absoluteImage = new BitmapImage(absolutePath);
+            //Image = new WriteableBitmap(absoluteImage);
+
+            // But using a relative path with a WriteableBitmap doesn't work
+            Uri relativePath = new Uri(@"Images\missingProfile.jpg", UriKind.Relative);
+            BitmapImage relativeImage = new BitmapImage(relativePath);
+            Image = new WriteableBitmap(relativeImage);
+        }
+
+        //public BitmapImage Image { get; set; }
+
+        public WriteableBitmap Image { get; set; }
+
+
+    }
+}
