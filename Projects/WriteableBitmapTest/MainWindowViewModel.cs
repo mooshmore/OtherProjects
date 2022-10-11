6cc8a7f8142a1b5aae74dd7f246369502cedeb27
Pete <mooshmore@gmail.com>
2022-10-11 12:43:17 +0200

    K2 task

--- Projects/WriteableBitmapTest/MainWindowViewModel.cs
+++ Projects/WriteableBitmapTest/MainWindowViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media.Imaging;
 using ViewModelBaseNamespace;
 
@@ -23,10 +25,26 @@ namespace WriteableBitmapTest
             //BitmapImage absoluteImage = new BitmapImage(absolutePath);
             //Image = new WriteableBitmap(absoluteImage);
 
-            // But using a relative path with a WriteableBitmap doesn't work
-            Uri relativePath = new Uri(@"Images\missingProfile.jpg", UriKind.Relative);
+            //But using a relative path with a WriteableBitmap doesn't work
+            //new Uri("pack://application:,,,/images/jamsnaps-dark.png");
+
+            Uri relativePath = new Uri(@"pack://application:,,,/images/missingProfile.jpg");
             BitmapImage relativeImage = new BitmapImage(relativePath);
             Image = new WriteableBitmap(relativeImage);
+
+            //var path = new AssemblyName(Assembly.GetCallingAssembly().FullName).Name + ";component/" + @"Images\missingProfile.jpg";
+            //var uri = new Uri(path, UriKind.Relative);
+            //using (var stream = Application.GetResourceStream(uri)?.Stream)
+            //{
+            //    var source = new BitmapImage();
+            //    source.BeginInit();
+            //    source.CreateOptions = BitmapCreateOptions.None;
+            //    source.StreamSource = stream;
+            //    source.EndInit();
+            //    Image = new WriteableBitmap(source);
+            //}
+
+            //new Uri("pack://application:,,,/images/jamsnaps-dark.png");
         }
 
         //public BitmapImage Image { get; set; }
